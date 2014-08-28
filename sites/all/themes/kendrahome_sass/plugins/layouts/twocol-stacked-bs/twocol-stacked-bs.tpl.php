e8213faf569b27a59369a6a98918db279f01b854
iAugur <george.boobyer@gmail.com>
2014-08-28 18:24:40 +0100

    f_14 - updated 2 col stacked bs template

--- sites/all/themes/kendrahome_sass/plugins/layouts/twocol-stacked-bs/twocol-stacked-bs.tpl.php
+++ sites/all/themes/kendrahome_sass/plugins/layouts/twocol-stacked-bs/twocol-stacked-bs.tpl.php
@@ -1,12 +1,30 @@
+<?php
+/**
+ * @file
+ * Template for a 2 column panel layout.
+ *
+ * This template provides a very simple "two column" panel display layout.
+ *
+ * Variables:
+ * - $id: An optional CSS id to use for the layout.
+ * - $content: An array of content, each item in the array is keyed to one
+ *   panel of the layout. This layout supports the following sections:
+ */
+?>
+
 <div class="<?php print $classes ?>" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
-  <div class="row-fluid">
+  <div class="row-fluid container region1">
     <?php print $content['top']; ?>
   </div>
-  <div class="row-fluid">
-    <?php print $content['left']; ?>
-    <?php print $content['right']; ?>
+  <div class="row-fluid container region2">
+    <div class="col-md-8">
+      <?php print $content['left']; ?>
+    </div>
+    <div class="col-md-4">
+      <?php print $content['right']; ?>
+    </div>
   </div>
-  <div class="row-fluid">
+   <div class="row-fluid container region3" role="footer">
     <?php print $content['bottom']; ?>
   </div>
 </div>
